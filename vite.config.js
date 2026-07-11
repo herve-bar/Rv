f0527e40c2804c34c77b3e13d5546d123a3a9cd4
herve-bar <herve.epane@gmail.com>
2026-07-11 09:06:43 +0100

    Add PWA support with VitePWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Rv/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'RV - Gestion de comptoir',
+        short_name: 'RV',
+        description: 'Application de gestion de comptoir',
+        theme_color: '#78716c',
+        background_color: '#fafaf9',
+        display: 'standalone',
+        scope: '/Rv/',
+        start_url: '/Rv/',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        navigateFallback: '/Rv/index.html'
+      }
+    })
+  ],
 })
